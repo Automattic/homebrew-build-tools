4a284647e07a6f2cdb2f5d0cfdd936506e59d32c
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2021-05-28 17:04:33 -0600

    Add `configure`

---
+++ configure.rb
@@ -0,0 +1,15 @@
+class Configure < Formula
+  desc "A lightweight native tool for working with configuration files"
+  homepage "https://github.com/automattic/configure"
+  url "https://github.com/Automattic/configure/releases/download/0.6.1/configure-macos.zip"
+  sha256 "901bec3333149686c2f052ec768acae5f5e32add23604a196cdac3bce222f4ca"
+  license ""
+
+  def install
+      bin.install "configure"   
+  end
+
+  test do
+    system "#{bin}/configure", "--help"
+  end
+end
