b47bac20a99b4382aa2d1635dd9d9a066fc3c3f5
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2021-07-21 23:47:48 -0600

    Update hostmgr.rb

--- hostmgr.rb
+++ hostmgr.rb
@@ -2,19 +2,17 @@ class Hostmgr < Formula
   desc "A native binary for managing macOS CI VM hosts"
   homepage "https://github.com/automattic/homebrew-build-tools"
   url "https://github.com/Automattic/homebrew-build-tools/releases/download/0.4.0/hostmgr.tar.gz"
-  sha256 "48fdca087b7ade5c6df307788c4a3dfe4912eb786b220960eb9bcd4fab16d1ae"
+  sha256 "191960b7ec09987ce85bab066d95dddd0cbd3e9b30f449b18dac52045b57bbb9"
   license ""
  
   depends_on "libssh2"
   
   def install   
       bin.install "hostmgr"
-    
-      puts Dir.glob("*")
-    
-      mv "homebrew.mxcl.hostmgr.sync.plist", var/"hostmgr/homebrew.mxcl.hostmgr.sync.plist"
-      mv "homebrew.mxcl.hostmgr.git-mirror-sync.plist", var/"hostmgr/homebrew.mxcl.hostmgr.git-mirror-sync.plist"
-      mv "homebrew.mxcl.hostmgr.git-mirror-server.plist", var/"hostmgr/homebrew.mxcl.hostmgr.git-mirror-server.plist"
+
+      mv "com.automattic.hostmgr.sync.plist", var/"hostmgr/com.automattic.hostmgr.sync.plist"
+      mv "com.automattic.hostmgr.git-mirror-sync.plist", var/"hostmgr/com.automattic.hostmgr.git-mirror-sync.plist"
+      mv "com.automattic.hostmgr.git-mirror-server.plist", var/"com.automattic.hostmgr.git-mirror-server.plist"
   end
 
   test do
