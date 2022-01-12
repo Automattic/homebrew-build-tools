2ae8c3255739b670cb7d4ab4b0e5b2623ab076d4
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-01-12 14:28:41 -0700

    Update hostmgr.rb

--- hostmgr.rb
+++ hostmgr.rb
@@ -9,32 +9,6 @@ class Hostmgr < Formula
   
   def install   
       bin.install "hostmgr"
-
-      FileUtils.mkdir_p var/"hostmgr/"
-      FileUtils.mkdir_p etc/"hostmgr/"
-
-      mv "com.automattic.hostmgr.sync.plist", var/"hostmgr/com.automattic.hostmgr.sync.plist"
-      mv "com.automattic.hostmgr.git-mirror-sync.plist", var/"hostmgr/com.automattic.hostmgr.git-mirror-sync.plist"
-      mv "com.automattic.hostmgr.git-mirror-server.plist", var/"hostmgr/com.automattic.hostmgr.git-mirror-server.plist"
-  end
-
-  def caveats
-    <<~EOS
-      hostmgr is installed, but to run its services, you'll need to run:
-      
-      mv #{var}/hostmgr/*.plist ~/Library/LaunchAgents/
-      
-      to set up the launch agents. From there, they can be started using:
-      
-      launchctl load -w ~/Library/LaunchAgents/com.automattic.hostmgr.sync.plist
-      launchctl load -w ~/Library/LaunchAgents/com.automattic.hostmgr.git-mirror-sync.plist
-      launchctl load -w ~/Library/LaunchAgents/com.automattic.hostmgr.git-mirror-server.plist
-      
-      launchctl start com.automattic.hostmgr.sync.plist
-      launchctl start com.automattic.hostmgr.git-mirror-sync.plist
-      launchctl start com.automattic.hostmgr.git-mirror-server.plist
-      
-    EOS
   end
 
   test do
