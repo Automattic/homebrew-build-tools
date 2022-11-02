83426d83514c3215ad1150a9f762bcbce94856e9
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-11-02 17:09:16 -0600

    Update hostmgr.rb

--- hostmgr.rb
+++ hostmgr.rb
@@ -1,8 +1,8 @@
 class Hostmgr < Formula
   desc "A native binary for managing macOS CI VM hosts"
   homepage "https://github.com/automattic/homebrew-build-tools"
-  url "https://github.com/Automattic/hostmgr/releases/download/0.14.2/hostmgr.tar.gz"
-  sha256 "f0c6c6ae925718e8075989806ee7eb79e9fac67da4922369a9ee07520f09c304"
+  url "https://github.com/Automattic/hostmgr/releases/download/0.15.0-beta.7/hostmgr.tar.gz"
+  sha256 "52545d2feeb3b96e675ffb5e034365fa126990c0d6ee4169da97629eaf8411be"
   license ""
  
   depends_on "libssh2"
