635f1c65ffc7571cc9d419aee4ab2ce2e1e91edc
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-09-11 16:37:00 +0200

    Specify version for event horizon in each formula

--- event-horizon.rb
+++ event-horizon.rb
@@ -1,13 +1,13 @@
 class EventHorizon < Formula
   desc "A multi-language code generation tool for type-safe event tracking."
   homepage "https://github.com/Automattic/EventHorizon"
-  version "0.2.0"
   license ""
 
   on_macos do
     on_arm do
       url "https://github.com/Automattic/EventHorizon/releases/download/0.2.0/event-horizon-cli-darwin-arm64"
       sha256 "52fee4a3bd442929c8863d04b8bf1b6092bd43de9fcecccf8ac6a793e4316979"
+      version "0.2.0"
     end
   end
 
@@ -15,6 +15,7 @@ class EventHorizon < Formula
     on_intel do
       url "https://github.com/Automattic/EventHorizon/releases/download/0.2.0/event-horizon-cli-linux-amd64"
       sha256 "34335a9f2d93483b68f473120d8fe1f15e78813e2b8bb578173bcf2b4460c6c2"
+      version "0.2.0"
     end
   end
 
