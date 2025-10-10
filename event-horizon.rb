24f99d840b86e6d33819555a461f11774a70cfd6
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-10-10 15:45:25 +0200

    fix: Update Event Horizon to 0.2.2

--- event-horizon.rb
+++ event-horizon.rb
@@ -1,7 +1,7 @@
 class EventHorizon < Formula
   desc "A multi-language code generation tool for type-safe event tracking."
   homepage "https://github.com/Automattic/EventHorizon"
-  version "0.2.1"
+  version "0.2.2"
   license ""
 
   on_macos do
