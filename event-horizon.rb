f2f72d82248006f241b220748d45b7377690dafd
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-10-10 15:51:55 +0200

    fix2: Update Event Horizon to 0.2.2

--- event-horizon.rb
+++ event-horizon.rb
@@ -6,7 +6,7 @@ class EventHorizon < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/Automattic/EventHorizon/releases/download/0.2.1/event-horizon-cli-darwin-arm64",
+      url "https://github.com/Automattic/EventHorizon/releases/download/0.2.2/event-horizon-cli-darwin-arm64",
         using: :nounzip
       sha256 "9d7e4393a6544d6439de0f296419b207da194ce33f406aa856d0c00a920a5d26"
     end
@@ -14,7 +14,7 @@ class EventHorizon < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/Automattic/EventHorizon/releases/download/0.2.1/event-horizon-cli-linux-amd64",
+      url "https://github.com/Automattic/EventHorizon/releases/download/0.2.2/event-horizon-cli-linux-amd64",
         using: :nounzip
       sha256 "48df801b1a1fb02983abe73e6b4d88855be89b9d63401994c420f1b22e0cc978"
     end
