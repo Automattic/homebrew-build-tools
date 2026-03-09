391f2dafe109d4ef1cabcd0c495aa8ab1666958e
Michał Sikora <me@mehow.io>
2026-03-09 10:58:37 +0100

    Bump Event Horizon version to 0.3.6

--- event-horizon.rb
+++ event-horizon.rb
@@ -1,22 +1,22 @@
 class EventHorizon < Formula
   desc "A multi-language code generation tool for type-safe event tracking."
   homepage "https://github.com/Automattic/EventHorizon"
-  version "0.3.5"
+  version "0.3.6"
   license ""
 
   on_macos do
     on_arm do
-      url "https://github.com/Automattic/EventHorizon/releases/download/0.3.5/event-horizon-cli-darwin-arm64",
+      url "https://github.com/Automattic/EventHorizon/releases/download/0.3.6/event-horizon-cli-darwin-arm64",
         using: :nounzip
-      sha256 "4b07711d7183b68fb84094e2b0164c06157641269d4eceac1659d747d8340fd5"
+      sha256 "6ab4fd66eb4f082f1736af1a77f5be5d049ef3cdb8cdbb973ddf2a6bbf65b6a3"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Automattic/EventHorizon/releases/download/0.3.5/event-horizon-cli-linux-amd64",
+      url "https://github.com/Automattic/EventHorizon/releases/download/0.3.6/event-horizon-cli-linux-amd64",
         using: :nounzip
-      sha256 "88f375d92d047e2b3d527ade31ad5383c2b569b594f98314c013a395df4f676a"
+      sha256 "be8f74ec88738c275d39c39bb97b1249a3b1d1ccd2100eb61d7a00f348959118"
     end
   end
 
